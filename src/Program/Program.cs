5ef0bb9277cc80675c2f0a3dc44cbd04dc946978
[Faustino <[faustino.harriague@correo.ucu.edu.uy]>
2021-11-10 18:16:38 -0300

    no logre la piramide

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Library;
+using System.Collections.Generic;
 
 namespace Program
 {
@@ -7,13 +8,22 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            Person Maria=new Person("Maria",1);
+            Node n1 = new Node(Maria);
+            Person Rafaela=new Person("Rafaela",2);
+            Node n2 = new Node(Rafaela);
+            Person Juana=new Person("Juana",3);
+            Node n3 = new Node(Juana);
+            Person Violeta=new Person("Violeta",4);
+            Node n4 = new Node(Violeta);
+            Person Michaela=new Person("Michaela",5);
+            Node n5 = new Node(Michaela);
+            Person Rosa=new Person("Rosa",6);
+            Node n6 = new Node(Rosa);
+            Person Ronda=new Person("Ronda",7);
+            Node n7 = new Node(Ronda);
+            Person Elena=new Person("Elena",999);
+            Node n8 =new Node(Elena);
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
@@ -23,8 +33,24 @@ namespace Program
 
             n3.AddChildren(n6);
             n3.AddChildren(n7);
+            n4.AddChildren(n8);
 
             // visitar el árbol aquí
+           
+            
+            Console.WriteLine($" N1 Es hijo = {n1.IsChild}, N1 tiene hijo = {n1.HasChild}");
+            Console.WriteLine($" N4 Es hijo = {n4.IsChild}, N4 tiene hijo = {n4.HasChild}");
+            Console.WriteLine($" N8 Es hijo = {n8.IsChild}, N8 tiene hijo = {n8.HasChild}");
+
+            VisitorAge age=new VisitorAge();
+           int edadmayor=age.Visit(n1);
+            Console.WriteLine($"La edad mayor es {edadmayor}");
+
+            
+           
+           
+            
+            
         }
     }
 }
